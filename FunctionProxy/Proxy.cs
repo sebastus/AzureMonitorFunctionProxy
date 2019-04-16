625325b5f52bb000e81aaca11597b9788a50e151
Greg Oliver <golive@microsoft.com>
2019-04-16 13:59:07 +0100

    add obHec to proxy

--- FunctionProxy/Proxy.cs
+++ FunctionProxy/Proxy.cs
@@ -1,12 +1,19 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace FunctionProxy
 {
@@ -20,12 +27,97 @@ namespace FunctionProxy
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+
+            await obHEC(requestBody, log);
+
             //dynamic data = JsonConvert.DeserializeObject(requestBody);
             //name = name ?? data?.name;
 
             return requestBody != null
-                ? (ActionResult)new OkObjectResult($"{requestBody}")
-                : new BadRequestObjectResult("No request body");
+                ? (ActionResult)new OkObjectResult($"Splunk event messages were passed along.")
+                : new BadRequestObjectResult("No messages were submitted to pass.");
+        }
+
+        public static string getEnvironmentVariable(string name)
+        {
+            var result = System.Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Process);
+            if (result == null)
+                return "";
+
+            return result;
+        }
+
+        static string splunkCertThumbprint { get; set; }
+
+        public class SingleHttpClientInstance
+        {
+            private static readonly HttpClient HttpClient;
+
+            static SingleHttpClientInstance()
+            {
+                HttpClient = new HttpClient();
+            }
+
+            public static async Task<HttpResponseMessage> SendToSplunk(HttpRequestMessage req)
+            {
+                HttpResponseMessage response = await HttpClient.SendAsync(req);
+                return response;
+            }
+        }
+
+        public static bool ValidateMyCert(object sender, X509Certificate cert, X509Chain chain, SslPolicyErrors sslErr)
+        {
+            // if user has not configured a cert, anything goes
+            if (string.IsNullOrWhiteSpace(splunkCertThumbprint))
+                return true;
+
+            // if user has configured a cert, must match
+            var thumbprint = cert.GetCertHashString();
+            if (thumbprint == splunkCertThumbprint)
+                return true;
+
+            return false;
+        }
+
+        public static async Task obHEC(String newClientContent, ILogger log)
+        {
+            splunkCertThumbprint = getEnvironmentVariable("splunkCertThumbprint");
+            string splunkAddress = getEnvironmentVariable("splunkAddress");
+            string splunkToken = getEnvironmentVariable("splunkToken");
+            if (splunkAddress.Length == 0 || splunkToken.Length == 0)
+            {
+                log.LogError("Values for splunkAddress and splunkToken are required.");
+                return;
+            }
+
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            ServicePointManager.ServerCertificateValidationCallback += new RemoteCertificateValidationCallback(ValidateMyCert);
+
+            log.LogInformation(newClientContent);
+
+            var client = new SingleHttpClientInstance();
+            try
+            {
+                HttpRequestMessage req = new HttpRequestMessage(HttpMethod.Post, splunkAddress);
+                req.Headers.Accept.Clear();
+                req.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                req.Headers.Add("Authorization", "Splunk " + splunkToken);
+                req.Content = new StringContent(newClientContent.ToString(), Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await SingleHttpClientInstance.SendToSplunk(req);
+                if (response.StatusCode != HttpStatusCode.OK)
+                {
+                    throw new System.Net.Http.HttpRequestException($"StatusCode from Splunk: {response.StatusCode}, and reason: {response.ReasonPhrase}");
+                }
+            }
+            catch (System.Net.Http.HttpRequestException e)
+            {
+                throw new System.Net.Http.HttpRequestException("Sending to Splunk. Is Splunk service running?", e);
+            }
+            catch (Exception f)
+            {
+                throw new System.Exception("Sending to Splunk. Unplanned exception.", f);
+            }
         }
     }
 }
